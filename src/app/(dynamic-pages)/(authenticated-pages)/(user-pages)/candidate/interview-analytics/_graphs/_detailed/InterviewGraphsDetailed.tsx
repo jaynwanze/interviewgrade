73374afc2e355aa15beb2010cffb46b2d96d9508
Jay Nwanze <j.nwanze@gmail.com>
2025-02-25 23:59:14 +0000

    Refactored layout of analytics

--- src/app/(dynamic-pages)/(authenticated-pages)/(user-pages)/candidate/interview-analytics/_graphs/_detailed/InterviewGraphsDetailed.tsx
+++ src/app/(dynamic-pages)/(authenticated-pages)/(user-pages)/candidate/interview-analytics/_graphs/_detailed/InterviewGraphsDetailed.tsx
@@ -1,6 +1,8 @@
 'use client';
+import { Card, CardTitle } from '@/components/ui/card';
 import { InterviewAnalytics } from '@/types';
 import { motion } from 'framer-motion';
+import { ClipboardList } from 'lucide-react';
 import { AreaChartInteractiveOverallGrades } from './AreaChartInteractiveOverallGrades';
 import { BarChartInteractiveEvaluationScores } from './BarCharInteractiveEvaluationScores';
 import { RadialChartEvaluationsScoreAverages } from './RadialChartEvaluationsScoreAverages';
@@ -39,23 +41,39 @@ export function InterviewGraphsDetailed({
       initial="hidden"
       animate="visible"
     >
-      <div className="grid xs:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-5 items-center">
-        <motion.div className="sm:col-span-2" variants={itemVariants}>
+      <div className="grid md:grid-cols-1 lg:grid-cols-3 gap-6 mb-5 items-center">
+        {/* Total Sessions Count */}
+        <Card className="flex flex-col md:col-span-1 justify-center items-center h-full shadow-lg rounded-lg text-center p-6">
+          <ClipboardList className="w-10 h-10 text-blue-500" />
+          <CardTitle className="mt-2">Total Completed Sessions</CardTitle>
+          <div>
+            <p className="text-4xl font-bold text-gray-900">
+              {analyticsData.total_interviews || 0}
+            </p>
+            <p className="text-gray-500">Total completed interview sessions.</p>
+          </div>
+        </Card>
+
+        <motion.div
+          className="md:col-span-1 lg:col-span-2"
+          variants={itemVariants}
+        >
           <RadialChartEvaluationsScoreAverages
             avgEvaluationCriteriaScores={
               analyticsData.avg_evaluation_criteria_scores
             }
           />
         </motion.div>
-        <motion.div className="sm:col-span-2" variants={itemVariants}>
-          <AreaChartInteractiveOverallGrades
-            completedInterviewEvaluations={
-              analyticsData.completed_interview_evaluations
-            }
-          />
-        </motion.div>
       </div>
-      <motion.div className="lg:grid-cols-2" variants={itemVariants}>
+      <motion.div className="" variants={itemVariants}>
+        <AreaChartInteractiveOverallGrades
+          completedInterviewEvaluations={
+            analyticsData.completed_interview_evaluations
+          }
+        />
+      </motion.div>
+
+      <motion.div className="" variants={itemVariants}>
         <BarChartInteractiveEvaluationScores
           completedInterviewEvaluations={
             analyticsData.completed_interview_evaluations
--- src/components/Interviews/InterviewAnalytics/InterviewAverageDetails.tsx
+++ src/components/Interviews/InterviewAnalytics/InterviewAverageDetails.tsx
@@ -16,7 +16,7 @@ export const InterviewAverageDetails = ({
   return (
     <>
       {/* Key Metrics Grid */}
-      <div className="grid xs:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-5 items-center">
+      <div className="grid xs:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-5 items-center">
         {/* Left Column - Interview Info + Overall Score */}
         <InterviewLatestCard latestInterview={latestInterview} />
         {/* Overall Grade + Best Skill Badge */}
@@ -37,21 +37,8 @@ export const InterviewAverageDetails = ({
             </p> */}
           </div>
         </Card>
-
-        {/* Total Sessions Count */}
-        <Card className="flex flex-col justify-center items-center h-full shadow-lg rounded-lg text-center p-6">
-          <ClipboardList className="w-10 h-10 text-blue-500" />
-          <CardTitle className="mt-2">Total Completed Sessions</CardTitle>
-          <div>
-            <p className="text-4xl font-bold text-gray-900">
-              {analyticsData.total_interviews || 0}
-            </p>
-            <p className="text-gray-500">Total completed interview sessions.</p>
-          </div>
-        </Card>
-
         {/* Average Mark Per Question */}
-        <Card className="flex flex-col justify-center items-center h-full shadow-lg rounded-lg text-center p-6">
+        <Card className="flex flex-col md:col-span-2 lg:col-span-1 justify-center items-center h-full shadow-lg rounded-lg text-center p-6">
           <Star className="w-10 h-10 text-purple-500" />
           <CardTitle className="mt-2">Average Score Per Question</CardTitle>
           <div>

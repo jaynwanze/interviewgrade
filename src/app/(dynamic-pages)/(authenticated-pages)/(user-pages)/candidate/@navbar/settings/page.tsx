93d6d02f775e267bb7dde082ae16df5d7ef56bee
Jay Nwanze <j.nwanze@gmail.com>
2025-04-29 18:33:05 +0100

    Ensuring title only shows within navabar when in settings dir

--- src/app/(dynamic-pages)/(authenticated-pages)/(user-pages)/candidate/@navbar/settings/page.tsx
+++ src/app/(dynamic-pages)/(authenticated-pages)/(user-pages)/candidate/@navbar/settings/page.tsx
@@ -1,7 +1,15 @@
+'use client';
+
 import { T } from '@/components/ui/Typography';
 import { cn } from '@/utils/cn';
+import { usePathname } from 'next/navigation';
 
 export default function InvitationsNavbar() {
+  const pathname = usePathname();
+  // Render only if the current path matches the settings page
+  if (!pathname.includes('/candidate/settings')) {
+    return null;
+  }
   return (
     <div className={cn('hidden lg:block', 'relative ')}>
       <T.P className="my-0">Account Settings</T.P>

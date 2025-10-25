6bbd78f33a3ea92d58acf907106b6316ee127fd0
“Jay” <j.nwanze@gmail.com>
2025-10-25 17:16:45 +0100

    Fix import paths in OrganizationSwitcher

--- src/app/(dynamic-pages)/(authenticated-pages)/(application-pages)/_sidebar/_components/OrganizationSwitcher.tsx
+++ src/app/(dynamic-pages)/(authenticated-pages)/(application-pages)/_sidebar/_components/OrganizationSwitcher.tsx
@@ -1,6 +1,6 @@
 'use client';
 // import { CreateOrganizationDialog } from '@/components/CreateOrganizationDialog';
-import { Button } from 'src/components/ui/button';
+import { Button } from '@/components/ui/button';
 import {
   Command,
   CommandEmpty,
@@ -8,9 +8,9 @@ import {
   CommandItem,
   CommandList,
   CommandSeparator,
-} from 'src/components/ui/command';
-import { createOrganization } from 'src/data/user/organizations';
-import { useSAToastMutation } from 'src/hooks/useSAToastMutation';
+} from '@/components/ui/command';
+import { createOrganization } from '@/data/user/organizations';
+import { useSAToastMutation } from '@/hooks/useSAToastMutation';
 import { cn } from '@/utils/cn';
 import {
   Popover,
